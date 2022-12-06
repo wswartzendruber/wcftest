64aa61cdf5652b7b23387d263dc35f42c9038935
William Swartzendruber <wswartzendruber@gmail.com>
2022-12-06 13:54:47 -0700

    Stripping some unneccessary listeners out.

--- Program.cs
+++ Program.cs
@@ -15,20 +15,15 @@ builder.Services.AddSingleton<IServiceBehavior, UseRequestHeadersForMetadataAddr
 
 var app = builder.Build();
 
-// Configure an explicit none credential type for WSHttpBinding as it defaults to Windows which requires extra configuration in ASP.NET
-var myWSHttpBinding = new WSHttpBinding(SecurityMode.Transport);
-myWSHttpBinding.Security.Transport.ClientCredentialType= HttpClientCredentialType.None;
-
 app.UseServiceModel(builder =>
 {
-    builder.AddService<EchoService>((serviceOptions) => { })
-    // Add a BasicHttpBinding at a specific endpoint
-    .AddServiceEndpoint<EchoService, IEchoService>(new BasicHttpBinding(), "/EchoService/basichttp")
-    // Add a WSHttpBinding with Transport Security for TLS
-    .AddServiceEndpoint<EchoService, IEchoService>(myWSHttpBinding, "/EchoService/WSHttps");
+    builder
+        .AddService<EchoService>((serviceOptions) => { })
+        .AddServiceEndpoint<EchoService, IEchoService>(new BasicHttpBinding(), "/EchoService");
 });
 
 var serviceMetadataBehavior = app.Services.GetRequiredService<CoreWCF.Description.ServiceMetadataBehavior>();
+
 serviceMetadataBehavior.HttpGetEnabled = true;
 
 app.Run();
